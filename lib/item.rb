2c4aede6ea58fa5ab8afca5d5ac864aaaa429e57
Luciano Oliveira and safiy cham <luciano@>
2017-09-20 09:22:20 -0700

    add passing spec to save items to class variable @@.

--- lib/item.rb
+++ lib/item.rb
@@ -1,8 +1,17 @@
 class Item
   @@list = []
 
-  
+  attr_accessor :name
+
+  def initialize(namex)
+    @name = namex
+  end
+
   def self.all()
     @@list
+  end
+
+  def save
+    @@list.push(self)
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,4 +6,12 @@ describe('Item') do
       expect(Item.all).to(eq([]))
     end
   end
+
+  describe("#save") do
+    it("saves an item to the list of items") do
+      item = Item.new("tacos")
+      item.save
+      expect(Item.all).to(eq([item]))
+    end
+  end
 end
