f6a71b78b7006f3c91783d7a77679694376296be
Luciano Oliveira and safiy cham <safiy@>
2017-09-20 11:12:46 -0700

    add passing spec to see if instances are the same if they have the same values.

--- lib/item.rb
+++ lib/item.rb
@@ -1,14 +1,25 @@
 class Item
   @@list = []
 
+  attr_reader :id
   attr_accessor :name
 
-  def initialize(namex)
-    @name = namex
+  def initialize(name)
+    @name = name
+    @id = @@list.length + 1
   end
 
   def self.all()
     @@list
+  end
+
+  def self.find(id)
+    item_id = id.to_i()
+    @@list.each do |item|
+      if item.id == item_id
+        return item
+      end
+    end
   end
 
   def save
