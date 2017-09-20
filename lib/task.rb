9cd924894f532b95e8f53164d49a986929518df6
Luciano Oliveira and safiy cham <luciano@>
2017-09-20 11:44:43 -0700

    add passing spec to save inputed values.

--- lib/task.rb
+++ lib/task.rb
@@ -5,7 +5,22 @@ class Task
     @description = attributes.fetch(:description)
   end
 
+  def self.all
+    returned_tasks = DB.exec("SELECT * FROM tasks;")
+    tasks = []
+    returned_tasks.each() do |task|
+      description = task.fetch("description")
+      # list_id = task.fetch("list_id").to_i() # The information comes out of the database as a string.
+      tasks.push(Task.new({:description => description}))
+    end
+    tasks
+  end
+
   def ==(another_task)
     self.description().==(another_task.description())
+  end
+
+  def save
+    DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}');")
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -12,17 +12,25 @@ RSpec.configure do |config|
 end
 
 describe(Task) do
-describe(".all") do
-  it("is empty at first") do
-    expect(Task.all)
+  describe(".all") do
+    it("is empty at first") do
+      expect(Task.all).to(eq([]))
+    end
   end
-end
 
   describe("#==") do
     it("is the same task if it has the same description") do
       task1 = Task.new({:description => "learn SQL"})
       task2 = Task.new({:description => "learn SQL"})
       expect(task1).to(eq(task2))
+    end
+  end
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Task.new({:description => "learn SQL"})
+      test_task.save
+      expect(Task.all).to(eq([test_task]))
     end
   end
 end
